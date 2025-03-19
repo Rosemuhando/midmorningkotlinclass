7526d0f144315602fa18fabbb89d2cb529257e74
rosemuhando <rosemuhando90@gmail.com>
2025-03-19 11:23:11 +0300

    my first commit

---
+++ src/datatypes.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var x = 55 //int
+    var number = 56.98 //double
+    var mydecimal = 45.87F //Float
+    var school = "emobilis" //string
+    var grade = 'A'//character
+    var ismammal = true //boolean
+    println(x)
+    println( number)
+    println( mydecimal)
+    println( school)
+    println( grade)
+    println(ismammal)
+
+
+}
